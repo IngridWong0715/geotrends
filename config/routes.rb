203781a23ed8eca86b38b947dc605ffca47d6d31
ingridwong0715 <ingrid830715@gmail.com>
2018-05-22 17:51:29 -0400

    Working code. NEXT STEP: Integrate redux

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/trending', to: 'tweets#trending'
-    get '/:query', to: 'tweets#search'
+    get '/trending/:query', to: 'tweets#search'
 
   end
 end
