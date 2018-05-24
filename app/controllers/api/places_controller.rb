57de91a97c3cfa8f24b9e188c6babfbd898ac75e
ingridwong0715 <ingrid830715@gmail.com>
2018-05-23 21:08:45 -0400

    Regenerate secret and put in .env

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,20 +1,20 @@
 require 'twitter'
 
 class Api::PlacesController < ApplicationController
- @@client = Twitter::REST::Client.new do |config|
-   config.consumer_key        = "A87Nzf0zVRiRv8CcNsMUJHhOP"
-   config.consumer_secret     = "xiv57E1eYZ5UBT6BV29d9wpV2lfgsblWEey5mK69QCtZdHzgCd"
-   config.access_token        = "2998573990-Y7areUjXnXhTJXA8CIKadDLbpmvwmQJmgWT0WG9"
-   config.access_token_secret = "lj3Q5yFHWXdLZiRhHeZtH7PdgPbnfxdHG84r38t1u6WWB"
- end
+  @@client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["CONSUMER_KEY"]
+    config.consumer_secret     = ENV["CONSUMER_SECRET"]
+    config.access_token        = ENV["ACCESS_TOKEN"]
+    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  end
 
 
   def available_places #GIVES THIS TO INPUT FORM:
     available_locations = @@client.trends_available #Returns the locations that Twitter has trending topic information for
-    available_locations.each do |place|
+    #available_locations.each do |place|
       #HMMMM THIS SEEMS REALLY DUMB....
-      Place.create(name: place.name, parentid: place.parent_id, country: place.country, woeid: place.woeid, countryCode: place.country_code, twitter_id: place.id)
-    end
+      #Place.create(name: place.name, parentid: place.parent_id, country: place.country, woeid: place.woeid, countryCode: place.country_code, twitter_id: place.id)
+    #end
     render json: available_locations
   end
 
--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,21 +1,19 @@
 require 'twitter'
 
 class Api::TweetsController < ApplicationController
+
  @@client = Twitter::REST::Client.new do |config|
-   config.consumer_key        = "A87Nzf0zVRiRv8CcNsMUJHhOP"
-   config.consumer_secret     = "xiv57E1eYZ5UBT6BV29d9wpV2lfgsblWEey5mK69QCtZdHzgCd"
-   config.access_token        = "2998573990-Y7areUjXnXhTJXA8CIKadDLbpmvwmQJmgWT0WG9"
-   config.access_token_secret = "lj3Q5yFHWXdLZiRhHeZtH7PdgPbnfxdHG84r38t1u6WWB"
+   config.consumer_key        = ENV["CONSUMER_KEY"]
+   config.consumer_secret     = ENV["CONSUMER_SECRET"]
+   config.access_token        = ENV["ACCESS_TOKEN"]
+   config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
  end
+
   def trending_by_location
-
     woeid = twitter_params[:woeid]
-
     trending_topics = @@client.trends(id=woeid).take(10)
     render json: trending_topics
   end
-
-
 
 
   private
