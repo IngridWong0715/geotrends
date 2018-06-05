f3af2f4129a38f90ce3cc44d8246919831e0ea45
ingridwong0715 <ingrid830715@gmail.com>
2018-06-05 16:47:52 -0400

    Configure in app.rb to serve static asset for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,8 @@ module GlobalTrendingTweetsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #For Heroku deployment
+    config.serve_static_assets = true
   end
 end
